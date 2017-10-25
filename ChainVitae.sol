ac4e359224120c6c29ac5bc130dbb12142a3c683
secure12 <trbhei@gmail.com>
2017-10-25 23:01:38 +0800

    changed storage method of main solidity

--- ChainVitae.sol
+++ ChainVitae.sol
@@ -1,89 +1,13 @@
 pragma solidity ^0.4.17;
-
-contract HashList{
-    
-    struct node{
-        bytes32 previous;
-        bytes32 hash;
-        bytes32 next;
-        bool valid;
-    }
-    
-    mapping(bytes32 => node) records;
-    function getRecord(bytes32 hash) public constant returns (bytes32){
-        return records[hash].hash;
-    }
-    function getNext(bytes32 hash) public constant returns (bytes32){
-        return records[hash].next;
-    }
-    function getPrevious(bytes32 hash) public constant returns (bytes32){
-        return records[hash].previous;
-    }
-    bytes32 head;
-    function getHead() public constant returns (bytes32){
-        return head;
-    }
-    bytes32 tail;
-    function getTail() public constant returns (bytes32){
-        return tail;
-    }
-    uint _size;
-    function size() public constant returns (uint){
-        return _size;
-    }
-    
-    function insert(bytes32 hash) public{
-        require(!records[hash].valid);
-        records[hash] = node({
-            previous: tail,
-            hash: hash,
-            next: 0x0,
-            valid: true
-        });
-        if (head == 0x0 && tail == 0x0){
-            head = hash;
-        }
-        else{
-            records[tail].next = hash; 
-        }
-        tail = hash;
-        _size++;
-    }
-    
-    function remove(bytes32 hash) public{
-        require(records[hash].valid);
-        _size--;
-
-        if (head != hash){
-            records[records[hash].previous].next = records[hash].next;
-        }
-        else{
-            head = records[hash].next;
-        }
-
-        if (tail != hash){
-            records[records[hash].next].previous = records[hash].previous;
-        }
-        else{
-            tail = records[hash].previous;
-        }
-        records[hash].valid = false;
-        delete records[hash];
-    }
-    
-    bytes32[] arr;
-    function iter() public returns (bytes32[]){
-        arr.length = 0;
-        bytes32 current = head;
-        while (current != 0x0){
-            arr.push(records[current].hash);
-            current = records[current].next;
-        }
-        return arr;
-    }
-}
-
 contract ChainVitae{
+
+    struct node{
+        bytes32 prev0;
+        bytes32 prev1;
+        vitae data;
+        bytes32 next0;
+        bytes32 next1;
+    }
 
     struct date{
         uint8 day;
@@ -100,17 +24,17 @@ contract ChainVitae{
         date endDate;
     }
 
-    mapping(address => address) vitaes;
-    mapping(address => address) pending;
-    mapping(address => address) requests;
-    
-    mapping(address => string) employee;
-    mapping(string => address) institution;
-    mapping(bytes32 => vitae) _vitaes;
+    mapping(address => mapping(uint8 => bytes32)) head;
+    mapping(address => mapping(uint8 => bytes32)) tail;
+    mapping(bytes32 => node) records;
 
-    function request(string institutionName, string positionName, uint8 startDay, uint8 startMonth, uint startYear, uint8 endDay, uint8 endMonth, uint endYear) public returns(bytes32){
+    mapping(address => string) employee;
+    mapping(address => string) institution;
+    mapping(bytes32 => vitae) vitaes;
+
+    function request(address institutionAddr, string positionName, uint8 startDay, uint8 startMonth, uint startYear, uint8 endDay, uint8 endMonth, uint endYear) public returns(bytes32){
         require(isEmployee());
-        require(institution[institutionName] != 0);
+        require(keccak256(institution[institutionAddr]) != keccak256(""));
         require(endYear >= startYear);
         if (endYear == startYear){
             require(endMonth >= startMonth);
@@ -119,8 +43,8 @@ contract ChainVitae{
             }
         }
         bytes32 hash = keccak256(
-            institutionName,
-            institution[institutionName],
+            institutionAddr,
+            institution[institutionAddr],
             employee[msg.sender],
             msg.sender,
             positionName,
@@ -129,77 +53,130 @@ contract ChainVitae{
             startYear,
             endDay,
             endMonth,
-            endYear);
-            
-        _vitaes[hash] = vitae({
-            institutionName: institutionName,
-            employeeName: employee[msg.sender],
-            employee: msg.sender,
-            positionName: positionName, 
-            startDate: date({
-                day: startDay,
-                month: startMonth,
-                year: startYear
-            }), 
-            endDate: date({
-                day: endDay,
-                month: endMonth,
-                year: endYear
-            })
+            endYear
+        );
+        records[hash] = node({
+            prev0:  tail[msg.sender][0],
+            prev1:  tail[institutionAddr][0],
+            data:
+                vitae({
+                    institutionName: institution[institutionAddr],
+                    employeeName: employee[msg.sender],
+                    employee: msg.sender,
+                    positionName: positionName, 
+                    startDate: date({
+                        day: startDay,
+                        month: startMonth,
+                        year: startYear
+                    }), 
+                    endDate: date({
+                        day: endDay,
+                        month: endMonth,
+                        year: endYear
+                    })
+                }),
+            next0:  0,
+            next1:  0
         });
-        HashList(pending[msg.sender]).insert(hash);
-        HashList(requests[institution[institutionName]]).insert(hash);
+        if (head[msg.sender][0] == 0){
+            head[msg.sender][0] = hash;
+        }
+        if (head[institutionAddr][0] == 0){
+            head[institutionAddr][0] = hash;
+        }
+        if (tail[msg.sender][0] != 0){
+            records[tail[msg.sender][0]].next0 = hash;
+        }
+        tail[msg.sender][0] = hash;
+        if (tail[institutionAddr][0] != 0){
+            records[tail[institutionAddr][0]].next1 = hash;
+        }
+        tail[institutionAddr][0] = hash;
         return hash;
     }
     
     function endorse(bytes32 hash) public{
-        require(institution[_vitaes[hash].institutionName] == msg.sender);
-        address employee = _vitaes[hash].employee;
-        HashList(pending[employee]).remove(hash);
-        HashList(requests[msg.sender]).remove(hash);
-        HashList(vitaes[employee]).insert(hash);
+        require(keccak256(institution[msg.sender]) == keccak256(records[hash].data.institutionName));
+        address employeeAddr = records[hash].data.employee;
+        bytes32 temp0 = records[hash].prev0;
+        bytes32 temp1 = records[hash].prev1;
+        bytes32 temp2 = records[hash].next0;
+        bytes32 temp3 = records[hash].next1;
+
+        if (temp0 != 0){
+            records[temp0].next0 = temp2;
+        }
+        else{
+            head[employeeAddr][0] = temp2;
+        }
+        if (temp1 != 0){
+            records[temp1].next1 = temp3;
+        }
+        else{
+            head[msg.sender][0] = temp3;
+        }
+        if (temp2 != 0){
+            records[temp2].prev0 = temp0;
+        }
+        else{
+            tail[employeeAddr][0] = temp0;
+        }
+        if (temp3 != 0){
+            records[temp3].prev1 = temp1;
+        }
+        else{
+            tail[msg.sender][0] = temp1;
+        }
+        if (head[employeeAddr][1] == 0){
+            head[employeeAddr][1] = hash;
+        }
+        if (head[msg.sender][1] == 0){
+            head[msg.sender][1] = hash;
+        }
+        if (tail[employeeAddr][1] != 0){
+            records[tail[employeeAddr][1]].next0 = hash;
+        }
+        tail[employeeAddr][1] = hash;
+        if (tail[msg.sender][1] != 0){
+            records[tail[msg.sender][1]].next1 = hash;
+        }
+        tail[msg.sender][1] = hash;
     }
     
-    function registerEmployee(string employeeName) public returns (address){
+    function registerEmployee(string name) public{
         require(!isEmployee());
-        require(institution[employeeName] == 0x0);
-        employee[msg.sender] = employeeName;
-        pending[msg.sender] = new HashList();
-        vitaes[msg.sender] = new HashList();
-        return pending[msg.sender];
+        employee[msg.sender] = name;
     }
     
     function isEmployee() public returns (bool){
-        return (isEmployee(msg.sender));
+        return (keccak256(employee[msg.sender]) != keccak256(""));
     }
     
     function isEmployee(address addr) public returns (bool){
         return (keccak256(employee[addr]) != keccak256(""));
     }
     
-    function registerInstitution(string institutionName) public{
-        require(keccak256(employee[msg.sender]) == keccak256(""));
-        require(institution[institutionName] == 0);
-        institution[institutionName] = msg.sender;
-        requests[msg.sender] = new HashList();
+    function registerInstitution(string name) public{
+        require(!isInstitution());
+        institution[msg.sender] = name;
     }
     
     function isInstitution() public returns (bool){
-        return (isInstitution(msg.sender));
+        return (keccak256(institution[msg.sender]) != keccak256(""));
     }
     
     function isInstitution(address addr) public returns (bool){
-        return (true);
+        return (keccak256(institution[addr]) != keccak256(""));
     }
     
     bytes32[] arr;
     function getPending() public returns (bytes32[]){
         require(isEmployee());
         arr.length = 0;
-        bytes32 current = HashList(pending[msg.sender]).getHead();
+        bytes32 current = head[msg.sender][0];
         while(current != 0x0){
-            arr.push(HashList(pending[msg.sender]).getRecord(current));
-            current = HashList(pending[msg.sender]).getNext(current);
+            arr.push(current);
+            current = records[current].next0;
         }
         return arr;
     }
@@ -207,10 +184,10 @@ contract ChainVitae{
     function getRequests() public returns (bytes32[]){
         require(isInstitution());
         arr.length = 0;
-        bytes32 current = HashList(requests[msg.sender]).getHead();
+        bytes32 current = head[msg.sender][0];
         while(current != 0x0){
-            arr.push(HashList(requests[msg.sender]).getRecord(current));
-            current = HashList(requests[msg.sender]).getNext(current);
+            arr.push(current);
+            current = records[current].next1;
         }
         return arr;
     }
@@ -218,11 +195,12 @@ contract ChainVitae{
     function getVitaes() public returns (bytes32[]){
         require(isEmployee());
         arr.length = 0;
-        bytes32 current = HashList(vitaes[msg.sender]).getHead();
+        bytes32 current = head[msg.sender][1];
         while (current != 0x0){
-            arr.push(HashList(vitaes[msg.sender]).getRecord(current));
-            current = HashList(vitaes[msg.sender]).getNext(current);
+            arr.push(current);
+            current = records[current].next0;
         }
         return arr;
     }
 }
+
